6c436f29386b55cbc0c7cc394c95d1e7d24ff50b
dryou0330 <62238035+dryou0330@users.noreply.github.com>
2020-11-26 00:32:45 +0900

    Add files via upload

---
+++ Script/Dragon/Dragon_Dool.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Dragon_Dool : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player; // 플레이어 벡터값 받아오기 
+    [SerializeField]
+    private GameObject move_Position; // 옮겨질 위치 
+    [SerializeField]
+    private GameObject dragon_First;
+
+    private void Start()
+    {
+        dragon_First.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            player.transform.position = move_Position.transform.position;
+            dragon_First.SetActive(true);
+        }
+    }
+}
---
+++ Script/ObjectPool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour // 탄피, 총알 , 총구 이펙트 , 몬스터 공격 (슬라임 , 터틀 , 드래곤)
+{
+    public static ObjectPool Instance;
+
+    [SerializeField]
+    private GameObject poolingObjectPrefab;
+
+    private Queue<GunController> poolingObjectQueue = new Queue<GunController>();
+    private void Awake()
+    {
+        Instance = this;
+        Initialize(10);
+    }
+
+    private GunController CreateNewObject()
+    {
+        var newObj = Instantiate(poolingObjectPrefab, transform).GetComponent<GunController>();
+        newObj.gameObject.SetActive(false);
+        return newObj;
+
+    }
+
+    private void Initialize(int _count)
+    {
+        for(int i = 0; i <_count; i++)
+        {
+            poolingObjectQueue.Enqueue(CreateNewObject());
+        }
+    }
+    public static GunController GetObject()
+    {
+        if(Instance.poolingObjectQueue.Count > 0)
+        {
+            var obj = Instance.poolingObjectQueue.Dequeue();
+            obj.transform.SetParent(null);
+            obj.gameObject.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            var newobj = Instance.CreateNewObject();
+            newobj.transform.SetParent(null);
+            newobj.gameObject.SetActive(true);
+            return newobj;
+        }
+    }
+    public static void ReturnObject(GunController bullet)
+    {
+        bullet.gameObject.SetActive(false);
+        bullet.transform.SetParent(Instance.transform);
+        Instance.poolingObjectQueue.Enqueue(bullet);
+    }
+
+}
